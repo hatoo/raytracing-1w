421dfefe7a2ddcaa608f923a535d6b8ed890407d
hatoo <hato2000@gmail.com>
2021-07-10 15:43:09 +0900

    Progress Indicator

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     println!("P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT);
 
     for j in (0..IMAGE_HEIGHT).rev() {
+        eprint!("\rScanlines remaining: {} ", j);
         for i in 0..IMAGE_WIDTH {
             let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
             let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
@@ -17,4 +18,6 @@ fn main() {
             println!("{} {} {}", ir, ig, ib);
         }
     }
+
+    eprintln!("\nDone");
 }
