c0365a85ec202f630ee9ea1537b52fd893d1347a
hatoo <hato2000@gmail.com>
2021-07-11 17:53:57 +0900

    Fix

--- src/sphere.rs
+++ src/sphere.rs
@@ -26,7 +26,7 @@ impl Hittable for Sphere {
         let mut root = (-half_b - sqrtd) / a;
         if root < t_min || t_max < root {
             root = (-half_b + sqrtd) / a;
-            if root < t_min || t_max > root {
+            if root < t_min || t_max < root {
                 return None;
             }
         }
