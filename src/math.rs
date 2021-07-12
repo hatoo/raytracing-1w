9eda5d9b5fd5aa5645fe807c0397003e57f77af0
hatoo <hato2000@gmail.com>
2021-07-12 15:53:17 +0900

    Fix warn

--- src/math.rs
+++ src/math.rs
@@ -16,6 +16,7 @@ pub fn random_vec3_in_unit_sphere(rng: &mut impl Rng) -> Vector3<Float> {
     }
 }
 
+#[allow(dead_code)]
 pub fn random_vec3_in_hemisphere(normal: Vector3<Float>, rng: &mut impl Rng) -> Vector3<Float> {
     let v = InnerSpace::normalize(random_vec3_in_unit_sphere(rng));
     if dot(normal, v) > 0.0 {
