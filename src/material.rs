5a0e07f2acf622850c9b9ea27ffb7aeb739eb854
hatoo <hato2000@gmail.com>
2021-07-12 15:09:47 +0900

    10.4

--- src/material.rs
+++ src/material.rs
@@ -2,6 +2,7 @@ use std::fmt::Debug;
 
 use crate::Float;
 use cgmath::{dot, vec3, InnerSpace, Vector3};
+use rand::Rng;
 
 use crate::{
     color::Color,
@@ -84,13 +85,19 @@ fn refract(uv: Vector3<Float>, n: Vector3<Float>, etai_over_etat: Float) -> Vect
     r_out_perp + r_out_parallel
 }
 
+fn reflectance(cosine: Float, ref_idx: Float) -> Float {
+    let r0 = (1.0 - ref_idx) / (1.0 + ref_idx);
+    let r0 = r0 * r0;
+    r0 + (1.0 - r0) * (1.0 - cosine).powf(5.0)
+}
+
 #[derive(Debug)]
 pub struct Dielectric {
     pub ir: Float,
 }
 
 impl Material for Dielectric {
-    fn scatter(&self, ray: &Ray, hit_record: &HitRecord, _rng: &mut MyRng) -> Option<Scatter> {
+    fn scatter(&self, ray: &Ray, hit_record: &HitRecord, rng: &mut MyRng) -> Option<Scatter> {
         let refraction_ratio = if hit_record.front_face {
             1.0 / self.ir
         } else {
@@ -102,11 +109,12 @@ impl Material for Dielectric {
         let sin_theta = (1.0 - cos_theta * cos_theta).sqrt();
         let cannot_refract = refraction_ratio * sin_theta > 1.0;
 
-        let direction = if cannot_refract {
-            reflect(unit_direction, hit_record.normal)
-        } else {
-            refract(unit_direction, hit_record.normal, refraction_ratio)
-        };
+        let direction =
+            if cannot_refract || reflectance(cos_theta, refraction_ratio) > rng.gen::<Float>() {
+                reflect(unit_direction, hit_record.normal)
+            } else {
+                refract(unit_direction, hit_record.normal, refraction_ratio)
+            };
 
         Some(Scatter {
             color: Color(vec3(1.0, 1.0, 1.0)),
