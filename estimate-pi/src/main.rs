9db00bfce61011797606af195aba80349285cdf3
hatoo <hato2000@gmail.com>
2021-07-14 14:26:09 +0900

    2.2

--- estimate-pi/src/main.rs
+++ estimate-pi/src/main.rs
@@ -1,16 +1,18 @@
 use rand::prelude::*;
 
 fn main() {
-    const N: usize = 1000;
     let mut rng = StdRng::from_entropy();
-    let inside_circle = (0..N)
-        .filter(|_| {
-            let x = rng.gen_range(-1.0..1.0);
-            let y = rng.gen_range(-1.0..1.0);
+    let mut inside_circle = 0;
+    for n in 0.. {
+        let x = rng.gen_range(-1.0..1.0);
+        let y = rng.gen_range(-1.0..1.0);
 
-            x * x + y * y < 1.0
-        })
-        .count();
+        if x * x + y * y < 1.0 {
+            inside_circle += 1;
+        }
 
-    println!("Estimate of Pi = {}", 4.0 * inside_circle as f64 / N as f64);
+        if n % 100000 == 0 {
+            println!("Estimate of Pi = {}", 4.0 * inside_circle as f64 / n as f64);
+        }
+    }
 }
