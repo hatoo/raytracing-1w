492dd9d936f1b112e0ce40a28a4b10cd2c6ee122
hatoo <hato2000@gmail.com>
2021-07-16 19:24:20 +0900

    final scene

--- src/main.rs
+++ src/main.rs
@@ -792,7 +792,7 @@ fn main() {
         5 => {
             aspect_ratio = 1.0;
             image_width = 600;
-            samples_per_pixel = 1000;
+            samples_per_pixel = 10000;
             (
                 cornel_box(&mut rng),
                 Color(vec3(0.0, 0.0, 0.0)),
